1c572262a21b98f2735361d96833fb22e5320aa5
basON <wch.basinski@gmail.com>
2022-07-05 10:38:04 +0200

    Added try catch to main.

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,28 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  const config = new DocumentBuilder()
-    .setTitle('Sparkledge')
-    .setDescription('Sparkledge backend endpoints')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_BUCKET_REGION,
-  });
-  app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true, transform: true }),
+    );
+    const config = new DocumentBuilder()
+      .setTitle('Sparkledge')
+      .setDescription('Sparkledge backend endpoints')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_BUCKET_REGION,
+    });
+    app.useGlobalFilters(new HttpExceptionFilter());
+    await app.listen(3000);
+  } catch (err) {
+    console.log(err.toString());
+  }
 }
 bootstrap();
